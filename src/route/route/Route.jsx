a43f578948f7c15ea7dba790b5ab7d4e736c847c
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-05-02 01:18:15 +0600

    completed the navbar

--- src/route/route/Route.jsx
+++ src/route/route/Route.jsx
@@ -1,11 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../../pages/home/Home";
+import Blog from "../../pages/blog/Blog";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Home></Home>,
+      children:[
+        {
+            path: "/blog",
+            element: <Blog></Blog>
+        }
+      ]
     },
+    {
+        path: "/login",
+        element: <div>this is log in path</div>
+    }
   ]);
 
 export default router;
--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -1,36 +1,75 @@
 import React from "react";
 import { Button, Container, Nav, Navbar, Form } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="white" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">
-          <span className="fw-bold">Dining</span> Pro
+        <Navbar.Brand href="#" className="fs-3">
+          <span className="fw-bold text-danger">Dining</span> Pro
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="mx-auto my-2 my-lg-0"
+            className="ms-auto my-2 my-lg-0"
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
             <NavLink
               to={`/`}
-              className={({ isActive }) => (isActive ? "fw-bold" : "")}
+              className={({ isActive }) =>
+                isActive
+                  ? "fw-bold me-3 text-danger"
+                  : "text-decoration-none text-dark me-3"
+              }
             >
               Home
             </NavLink>
             <NavLink
+              to="/chef"
+              className={({ isActive }) =>
+                isActive
+                  ? "fw-bold me-3 text-danger"
+                  : "text-decoration-none text-dark me-3"
+              }
+            >
+              Our Chef
+            </NavLink>
+            <NavLink
+              to={`/extra1`}
+              className={({ isActive }) =>
+                isActive
+                  ? "fw-bold me-3 text-danger"
+                  : "text-decoration-none text-dark me-3"
+              }
+            >
+              Extra1
+            </NavLink>
+            <NavLink
+              to={`/extra2`}
+              className={({ isActive }) =>
+                isActive
+                  ? "fw-bold me-3 text-danger"
+                  : "text-decoration-none text-dark me-3"
+              }
+            >
+              Extra2
+            </NavLink>
+            <NavLink
               to={`/blog`}
-              className={({ isActive }) => (isActive ? "fw-bold" : "")}
+              className={({ isActive }) =>
+                isActive
+                  ? "fw-bold me-3 text-danger"
+                  : "text-decoration-none text-dark me-3 "
+              }
             >
               Blog
             </NavLink>
           </Nav>
-
-          <Button variant="outline-success">Login</Button>
+          <Link to="/login">
+            <Button variant="btn btn-danger">Login</Button>
+          </Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
